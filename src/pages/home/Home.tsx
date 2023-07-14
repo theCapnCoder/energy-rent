a6c3ad1a470324aab89d08d5fc010b00e50842b4
PavelM <myagkiypavel@gmail.com>
2023-07-14 18:07:16 +0300

    chore: Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,36 @@
-import { Box, BoxProps } from "@mui/material";
-
-const homeStyle: BoxProps["sx"] = {
-  backgroundColor: "red",
-};
+import { Box, BoxProps, Paper } from "@mui/material";
+import { TopBox } from "../../components/topBox";
+import { ChartBox } from "../../components/chartBox";
 
 export const Home = () => {
-  return <Box sx={homeStyle}>Home</Box>;
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gap: 3,
+        gridTemplateColumns: "repeat(4, 1fr)",
+        gridAutoRows: "minmax(180px, auto)",
+      }}
+    >
+      <Paper sx={{ gridColumn: "span 1", gridRow: "span 3" }}>
+        <TopBox />
+      </Paper>
+      <Paper>
+        <ChartBox />
+      </Paper>
+      <Paper>
+        <ChartBox />
+      </Paper>
+      <Paper sx={{ gridColumn: "span 1", gridRow: "span 3" }}>
+        <ChartBox />
+      </Paper>
+      <Paper>
+        <ChartBox />
+      </Paper>
+      <Paper>6</Paper>
+      <Paper sx={{ gridColumn: "span 2", gridRow: "span 2" }}>7</Paper>
+      <Paper>8</Paper>
+      <Paper>9</Paper>
+    </Box>
+  );
 };
