952b35341cd5b2c97ca5d20959f294b706571374
PavelM <myagkiypavel@gmail.com>
2023-07-16 20:55:27 +0300

    chore: BarChartBox

--- src/components/barChartBox/BarChartBox.tsx
+++ src/components/barChartBox/BarChartBox.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import { Bar, BarChart, ResponsiveContainer } from "recharts";
+import { ChartBoxProps } from "../chartBox/ChartBox";
 
-export const BarChartBox = () => {
+export const BarChartBox = (props: ChartBoxProps) => {
   return (
-    <div>BarChartBox</div>
-  )
-}
+    <Box>
+      <Typography></Typography>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart width={150} height={40} data={data}>
+          <Bar dataKey="uv" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </Box>
+  );
+};
