8e725501dcd0c0f20c2f1ab53b1c3164d1796f08
PavelM <myagkiypavel@gmail.com>
2023-07-18 19:13:43 +0300

    style: fix barChartBox

--- src/components/barChartBox/BarChartBox.tsx
+++ src/components/barChartBox/BarChartBox.tsx
@@ -1,14 +1,24 @@
 import { Box, Typography } from "@mui/material";
 import { Bar, BarChart, ResponsiveContainer, Tooltip } from "recharts";
-import { ChartBoxProps } from "../chartBox/ChartBox";
 
-export const BarChartBox = (props: ChartBoxProps) => {
+export type BarChartBoxProps = {
+  title: string;
+  color: string;
+  dataKey: string;
+  chartData: object[];
+};
+
+export const BarChartBox = (props: BarChartBoxProps) => {
   return (
-    <Box>
+    <Box sx={{ width: "100%", height: "100%", p: 2 }}>
       <Typography>{props.title}</Typography>
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="90%" height={150}>
         <BarChart width={150} height={40} data={props.chartData}>
-          <Tooltip />
+          <Tooltip
+            contentStyle={{ background: "#2a3447", borderRadius: "5px" }}
+            labelStyle={{ display: "none" }}
+            cursor={{ fill: "none" }}
+          />
           <Bar dataKey={props.dataKey} fill={props.color} />
         </BarChart>
       </ResponsiveContainer>
