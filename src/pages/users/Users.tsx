0786c85ea857954ad7213d4466de8955e074d085
PavelM <myagkiypavel@gmail.com>
2023-07-19 17:27:12 +0300

    chore: Users

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,11 @@
+import { Box, Typography } from "@mui/material";
+import { DataTable } from "../../components/dataTable";
+
 export const Users = () => {
   return (
-    <div>Users</div>
-  )
-}
+    <Box>
+      <Typography variant="h4" my={2}>Users</Typography>
+      <DataTable />
+    </Box>
+  );
+};
