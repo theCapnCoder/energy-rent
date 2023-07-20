24c05a9bd8c06697869295c92d68029c9b52c83a
PavelM <myagkiypavel@gmail.com>
2023-07-20 16:09:39 +0300

    fix: dataUsers add type

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,60 +1,77 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Button, Stack, Typography } from "@mui/material";
 import { DataTable } from "../../components/dataTable";
 import { GridColDef } from "@mui/x-data-grid";
 import { userRows } from "../../data/data";
+import { useState } from "react";
+import { Add } from "../../components/add";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <Avatar alt="Remy Sharp" src={params.row.img} />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 150,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 170,
+  },
+  {
+    field: "createdAt",
+    type: "string",
+    headerName: "Created at",
+    width: 160,
+  },
+  {
+    field: "verified",
+    headerName: "Status",
+    width: 100,
+    type: "boolean",
+  },
+];
 
 export const Users = () => {
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "avatar",
-      headerName: "Avatar",
-      width: 100,
-      renderCell: (params) => {
-        return <Avatar alt="Remy Sharp" src={params.row.img} />;
-      },
-    },
-    {
-      field: "firstName",
-      headerName: "First name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "lastName",
-      headerName: "Last name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      width: 150,
-    },
-    {
-      field: "phone",
-      headerName: "Phone",
-      width: 170,
-    },
-    {
-      field: "createdAt",
-      headerName: "Created at",
-      width: 160,
-    },
-    {
-      field: "verified",
-      headerName: "Status",
-      width: 100,
-      type: "boolean",
-    },
-  ];
+  const [open, setOpen] = useState(false);
 
   return (
     <Box>
-      <Typography variant="h4" my={2}>
-        Users
-      </Typography>
+      <Stack direction={"row"} alignItems={"center"} gap={2}>
+        <Typography variant="h4" my={2}>
+          Users
+        </Typography>
+        <Button onClick={() => setOpen(true)} variant="contained">
+          Add new user
+        </Button>
+      </Stack>
       <DataTable slug="user" columns={columns} rows={userRows} />
+      {open && (
+        <Add slug="user" open={open} columns={columns} setOpen={setOpen} />
+      )}
     </Box>
   );
 };
