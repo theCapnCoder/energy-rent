73a92d3ce3953fc4cabd85e5571c3b7ecfd1bf2b
PavelM <myagkiypavel@gmail.com>
2023-07-20 16:09:49 +0300

    feature: add Add component

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,59 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { GridColDef } from "@mui/x-data-grid";
+import { isTemplateExpression } from "typescript";
+
+type Props = {
+  slug: string;
+  open: boolean;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const Add = (props: Props) => {
+  return (
+    <Dialog
+      open={props.open}
+      onClose={() => props.setOpen(false)}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <Box component="form">
+        <DialogTitle id="alert-dialog-title">Add new {props.slug}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <Box>
+              {props.columns
+                .filter((item) => item.field !== "id" && item.field !== "img")
+                .map((column) => (
+                  <TextField
+                    sx={{
+                      m: 1,
+                      width: "47%",
+                    }}
+                    name={column.field}
+                    label={column.headerName}
+                  />
+                ))}
+            </Box>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => props.setOpen(false)}>Close</Button>
+          <Button onClick={() => props.setOpen(false)} autoFocus>
+            Send
+          </Button>
+        </DialogActions>
+      </Box>
+    </Dialog>
+  );
+};
